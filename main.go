a8d05c54743dfa642e23172e493e835ef03f5436
steven12520 <song12520>
2020-03-20 15:17:14 +0800

    默认的

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	_ "./routers"
-	"./ziptest"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 )
 
 func main() {
 
-	ziptest.TestCompress()
+	//ziptest.TestCompress()
 	//ziptest.TestDeCompress()
 	logs.Debug("已启动。。。。。。。。。")
 	beego.Run()
